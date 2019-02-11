fd943581d93b69dbb4c8ac5e3566bb3afbf6edda
Hermann Hans <hahn.bra@gmail.com>
2019-02-11 17:30:49 -0200

    starting faq

---
+++ scripts/faq.js
@@ -0,0 +1,11 @@
+//CONVERT STRING IN NUMBER
+parseInt("123", 10);
+parseFloat("123");
+var parse = + "123";
+//Obs: If string is not a number, result is NaN
+//>var parse = "hello";
+//>isNaN(parse);
+//>true
+
+//CHECK IF NUNBER IS FINITE
+isFinite();
